0a424b54858dc560a9181e233594c75b7230364e
BruceZh <xzhong86@163.com>
2022-05-30 23:30:48 +0800

    fix bug

--- vps-update-blog.sh
+++ vps-update-blog.sh
@@ -6,9 +6,9 @@ html_dir=$2
 cd $HOME
 mkdir -p ./tmp
 
-[ -d ./tmp/$repo ] || rm -fr $HOME/tmp/$repo
+[ -d ./tmp/$repo ] && rm -fr $HOME/tmp/$repo
 
-git clone /git/$repo.git ./tmp/$repo
+git clone /git/$repo.git $HOME/tmp/$repo
 
 jekyll build -s ./tmp/$repo -d /var/www/html/$html_dir
 
